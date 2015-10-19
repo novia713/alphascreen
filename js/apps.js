f1c5b0af986975116f6120b9a4164f82bd416bb8
Leandro Vázquez Cervantes <leandro@leandro.org>
2015-10-19 17:55:48 +0200

    first settings implementation. has big bugs :(

--- js/apps.js
+++ js/apps.js
@@ -5,55 +5,112 @@
  * v. 20151017
  */
 (function() {
-    // Apps container
-    var parent = document.getElementById('apps');
 
+    // Apps container
+    var parent   = document.getElementById('apps');
+    var settings = document.getElementById('menu_settings');
     var iconMap = new WeakMap();
 
+    /* CONFIG */
 
 
-    /**
-     * Letters section generator
-     */
-    Array.apply(0, Array(26)).map(function(x, y) {
-        var l = String.fromCharCode(y + 65);
-        var letter = document.createElement('div');
-        letter.className = 'letter';
-        letter.id = l;
-        letter.innerHTML = "<div>" +l + "</div>";
-        letter.setAttribute("name", l);
-        // by default all letters are invisible,
-        // we make them visible if have icons afterwards
-        letter.style.display = "none";
-        parent.appendChild(letter);
-    });
+    var x = 2; //default icon columns key value
+    var config = {};
+
+    config.empty_letters = true;
+    config.columns = [];
+    config.columns[2]=65;
+    config.columns[3]=35;
 
 
-    /**
-     * Renders the icon to the container.
-     */
-    function render(icon) {
-        var name = icon.app.manifest.name;
-        var firstchar = name.charAt(0);
-        var tile = document.createElement('div');
+    /* END CONFIG */
 
-        tile.className = 'tile';
-        tile.style.background = 'url(' + icon.icon + ') center/65% no-repeat';
-        //FIXME
-        //tile.innerHTML = "<center><b class='appname'>" + name + "</b></center>";
-        document.getElementById(firstchar).appendChild(tile);
-        document.getElementById(firstchar).style.display = "block"
-        iconMap.set(tile, icon);
+
+
+
+    var start = function() {
+        console.log(x);
+        parent.innerHTML = "";
+
+        var skull = document.createElement('span');
+        skull.id = "settings";
+        skull.innerHTML = "☠";
+        parent.appendChild(skull);
+
+
+        app_status("block", "none");
+        /**
+         * Letters section generator
+         */
+        Array.apply(0, Array(26)).map(function(x, y) {
+            var l = String.fromCharCode(y + 65);
+            var letter = document.createElement('div');
+            letter.className = 'letter';
+            letter.id = l;
+            letter.innerHTML = "<div>" +l + "</div>";
+            letter.setAttribute("name", l);
+            if (config.empty_letters) {
+                letter.style.display = "none";
+            }
+            parent.appendChild(letter);
+        });
+
+
+        /**
+         * Renders the icon to the container.
+         */
+        function render(icon) {
+
+            var name = icon.app.manifest.name;
+            var wordname = name.split(" ");
+            var firstchar = name.charAt(0);
+            var tile = document.createElement('div');
+
+            tile.className = 'tile';
+            tile.className += ' icon_' + wordname[0];
+            tile.style.background = 'url(' + icon.icon + ') center/'+ config.columns[x]+'% no-repeat';
+            tile.style.width = (2 == x)? "125px" : "95px";
+
+            document.getElementById(firstchar).appendChild(tile);
+            document.getElementById(firstchar).style.display = "block";
+            iconMap.set(tile, icon);
+        }
+
+        /**
+         * Fetch all apps and render them.
+         */
+
+        FxosApps.all().then(icons => {
+            icons.forEach(render);
+
+        });
+
+
+        //options buttons
+        if (config.empty_letters == true) {
+            document.getElementById('btn_config__hide_empty_letters').disabled = true;
+            document.getElementById('btn_config__show_empty_letters').disabled = false;
+        }
+        if (config.empty_letters == false) {
+            document.getElementById('btn_config__show_empty_letters').disabled = true;
+            document.getElementById('btn_config__hide_empty_letters').disabled = false;
+        }
+
+        if (2 == x) {
+            document.getElementById('btn_config__icons_2_columns').disabled = true;
+            document.getElementById('btn_config__icons_3_columns').disabled = false;
+        }
+
+        if (3 == x) {
+            document.getElementById('btn_config__icons_2_columns').disabled = false;
+            document.getElementById('btn_config__icons_3_columns').disabled = true;
+        }
     }
 
-    /**
-     * Fetch all apps and render them.
-     */
-    FxosApps.all().then(icons => {
-
-        icons.forEach(render);
-    });
-
+    var app_status = function(state_apps, state_settings){
+        parent.style.display = state_apps;
+        settings.style.display = state_settings;
+    }
 
     /**
      * Add an event listener to launch the app on click.
@@ -61,10 +118,34 @@
     window.addEventListener('click', e => {
         var i = iconMap.get(e.target);
         if (i) i.launch();
-        else { //scrollbar
+        else {
+            console.log(e.target.id);
+            //open settings
+            if (e.target.id == "settings") {
+                app_status("none","block");
+            }
 
+            if (e.target.id == "btn_config__hide_empty_letters") {
+                config.empty_letters = true;
+                start();
+            }
 
+            if (e.target.id == "btn_config__show_empty_letters") {
+                config.empty_letters = false;
+                start();
+            }
+
+            if (e.target.id == "btn_config__icons_2_columns") {
+                x = 2;
+                start();
+            }
+
+            if (e.target.id == "btn_config__icons_3_columns") {
+                x = 3;
+                start();
+            }
 /*
+ *          //scrollbar
             var scroll_height = document.getElementById('scrollbar').scrollHeight;
 
             document.getElementById('scrollbar').style.marginTop = (scroll_height + e.clientY) + 'px';
@@ -76,5 +157,8 @@
 */
         }
     });
+
+
+    start();
 
 }());
